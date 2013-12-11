1fcf5f580630a0a47b6e76367ab860f687d3d983
Michał Sidor <michcioperz@autistici.org>
2013-12-11 21:06:15 +0100

    The code became more selfish now :(

--- bpmcnt.lua
+++ bpmcnt.lua
@@ -1,25 +1,25 @@
 BpmState = class('BpmState', State)
 
 function BpmState:initialize()
-	BpmState.bpmsum = 0
-	BpmState.delta = 0
-	BpmState.taps = -2
-	BpmState.paused = false
+	self.bpmsum = 0
+	self.delta = 0
+	self.taps = -2
+	self.paused = false
 end
 
 function BpmState:update(dt)
-	if not BpmState.paused and BpmState.taps > -2 then
-		BpmState.delta = BpmState.delta + dt
+	if not self.paused and self.taps > -2 then
+		self.delta = self.delta + dt
 	end
 end
 
 function BpmState:draw()
-	local bps = BpmState.taps / BpmState.bpmsum
+	local bps = self.taps / self.bpmsum
 	local bpm = bps * 60
-	local colorfactor = 255*BpmState.delta/bps
+	local colorfactor = 255*self.delta/bps
 	love.graphics.setColor(0,0,0,255)
 	love.graphics.rectangle("fill", 0, 0, 700, 700)
-	if BpmState.taps > 15 then
+	if self.taps > 15 then
 		love.graphics.setColor(colorfactor,colorfactor,colorfactor,255)
 	else
 		love.graphics.setColor(0,0,0,255)
@@ -27,22 +27,22 @@ function BpmState:draw()
 	love.graphics.rectangle("fill", 10, 10, 80, 680)
 	f:set(40)
 	love.graphics.setColor(255,255,255,255)
-	love.graphics.printf("sum:    "..BpmState.bpmsum, 100, 200, 300)
-	love.graphics.printf("taps:   "..BpmState.taps, 100, 300, 300)
+	love.graphics.printf("sum:    "..self.bpmsum, 100, 200, 300)
+	love.graphics.printf("taps:   "..self.taps, 100, 300, 300)
 	love.graphics.printf("BPM:    "..bpm, 100, 400, 300)
-	love.graphics.printf("offset: "..BpmState.delta, 100, 600, 300)
+	love.graphics.printf("offset: "..self.delta, 100, 600, 300)
 end
 
 function BpmState:keypressed(key, unicode)
 	if key == "s" then
-		BpmState.paused = not BpmState.paused
+		self.paused = not self.paused
 	else
-		if not BpmState.paused then
-			BpmState.taps = BpmState.taps + 1
-			if BpmState.taps > -1 then
-				BpmState.bpmsum = BpmState.bpmsum + BpmState.delta
+		if not self.paused then
+			self.taps = self.taps + 1
+			if self.taps > -1 then
+				self.bpmsum = self.bpmsum + self.delta
 			end
-			BpmState.delta = 0
+			self.delta = 0
 		end
 	end
 end
--- menu.lua
+++ menu.lua
@@ -1,19 +1,19 @@
 MenuState = class('MenuState', State)
 
 function MenuState:initialize()
-	MenuState.beattime = {0,{255,255,255,255}}
+	self.beattime = {0,{255,255,255,255}}
 end
 
 function MenuState:update(dt)
-	MenuState.beattime[1] = MenuState.beattime[1] + dt
-	if MenuState.beattime[1] >= 60/144 then
-		MenuState.beattime[1] = MenuState.beattime[1] - 60/144
-		MenuState.beattime[2][4] = 255-MenuState.beattime[1]*255
-		MenuState.beattime[2] = {255,255,255,MenuState.beattime[2][4]}
+	self.beattime[1] = self.beattime[1] + dt
+	if self.beattime[1] >= 60/144 then
+		self.beattime[1] = self.beattime[1] - 60/144
+		self.beattime[2][4] = 255-self.beattime[1]*255
+		self.beattime[2] = {255,255,255,self.beattime[2][4]}
 	else
-		MenuState.beattime[2][4] = 255-MenuState.beattime[1]*255
+		self.beattime[2][4] = 255-self.beattime[1]*255
 	end
-	print(MenuState.beattime[2][4])
+	print(self.beattime[2][4])
 end
 
 function MenuState:draw()
@@ -21,16 +21,16 @@ function MenuState:draw()
 	love.graphics.rectangle("fill", 0, 0, 700, 700)
 	love.graphics.setColor(255,255,255,255)
 	f:set(40)
-	love.graphics.setColor(0,255,0,MenuState.beattime[2][4])
+	love.graphics.setColor(0,255,0,self.beattime[2][4])
 	love.graphics.printf("Visuall", 50, 150, 600, "center")
 	x, y = love.mouse.getPosition()
-	love.graphics.setColor(255,0,0,MenuState.beattime[2][4])
+	love.graphics.setColor(255,0,0,self.beattime[2][4])
 	if x>= 50 and x<=300 and y>=300 and y<=400 then
 		love.graphics.rectangle("line", 50, 300, 250, 100)
 	else
 		love.graphics.rectangle("fill", 50, 300, 250, 100)
 	end
-	love.graphics.setColor(0,0,255,MenuState.beattime[2][4])
+	love.graphics.setColor(0,0,255,self.beattime[2][4])
 	if x>=400 and x<=650 and y>=300 and y<=400 then
 		love.graphics.rectangle("line", 400, 300, 250, 100)
 	else
