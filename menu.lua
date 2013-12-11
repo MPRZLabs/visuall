8740a5acb7db7598ed8754675fc35898f34ac082
Michał Sidor <michcioperz@autistici.org>
2013-12-11 20:12:40 +0100

    more menu upgrades

--- menu.lua
+++ menu.lua
@@ -22,10 +22,32 @@ function MenuState:draw()
 	love.graphics.setColor(255,255,255,255)
 	f:set(40)
 	love.graphics.setColor(0,255,0,MenuState.beattime[2][4])
-	love.graphics.printf("Visuall", 50, 50, 600, "center")
-	f:set(20)
+	love.graphics.printf("Visuall", 50, 150, 600, "center")
+	x, y = love.mouse.getPosition()
 	love.graphics.setColor(255,0,0,MenuState.beattime[2][4])
-	love.graphics.printf("BPM Counter", 50, 300, 250, "right")
+	if x>= 50 and x<=300 and y>=300 and y<=400 then
+		love.graphics.rectangle("line", 50, 300, 250, 100)
+	else
+		love.graphics.rectangle("fill", 50, 300, 250, 100)
+	end
 	love.graphics.setColor(0,0,255,MenuState.beattime[2][4])
-	love.graphics.printf("Run the beat", 400, 300, 250, "left")
+	if x>=400 and x<=650 and y>=300 and y<=400 then
+		love.graphics.rectangle("line", 400, 300, 250, 100)
+	else
+		love.graphics.rectangle("fill", 400, 300, 250, 100)
+	end
+	love.graphics.setColor(0,0,0,255)
+	love.graphics.printf("BPM Counter",   50, 300, 200, "right")
+	love.graphics.printf("Run the beat", 450, 300, 200, "left")
+end
+
+function MenuState:mousepressed(x,y,b)
+	if b == "l" then
+		if x>= 50 and x<=300 and y>=300 and y<=400 then
+			statechange(gamestates['beat'])
+		end
+		if x>=400 and x<=650 and y>=300 and y<=400 then
+			statechange(gamestates['visu'])
+		end
+	end
 end
