5826dd47b7ce3fc3ac5c87f6deecccccaabb6584
Michał Sidor <michcioperz@autistici.org>
2013-12-14 19:00:19 +0100

    Upgrading to LOVE 0.9.0

--- conf.lua
+++ conf.lua
@@ -2,10 +2,10 @@ function love.conf(t)
 	t.title = "Visuall"
 	t.author = "Michcioperz"
 	t.console = true
-	t.version = "0.8.0"
-	t.screen.width = 700
-	t.screen.height = 700
+	t.version = "0.9.0"
+	t.window.width = 700
+	t.window.height = 700
 	t.modules.joystick = false
 	t.modules.physics = false
-	t.screen.fullscreen = false
+	t.window.fullscreen = false
 end
